008f5dfa09d15a570e29bb7f046461e3aef4a723
Eugene Volkov <eugenewolf507@gmail.com>
2020-12-08 11:13:19 +0200

    Updates

---
+++ precache-manifest.1221b3156687bfb3e030c6743d1ff68a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc5831992496ae1ae22741e3a25bf330",
+    "url": "/goit-react-hw-05-expense-manager/index.html"
+  },
+  {
+    "revision": "4f8a24b9cba5b00fea30",
+    "url": "/goit-react-hw-05-expense-manager/static/css/main.951a195d.chunk.css"
+  },
+  {
+    "revision": "ddff9df079e2d2906389",
+    "url": "/goit-react-hw-05-expense-manager/static/js/2.eba91739.chunk.js"
+  },
+  {
+    "revision": "4f8a24b9cba5b00fea30",
+    "url": "/goit-react-hw-05-expense-manager/static/js/main.c5c38917.chunk.js"
+  },
+  {
+    "revision": "940793ec64ffc7578e1e",
+    "url": "/goit-react-hw-05-expense-manager/static/js/runtime~main.4bf5be61.js"
+  }
+]);
